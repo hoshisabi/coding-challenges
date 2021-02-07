f5fd2a7d78ad767544994b122124806f952c1d2c
Daniel E. Chapman II <chapmand@gmail.com>
2021-02-07 15:57:55 -0500

    Added unix test

---
+++ test/pe/PEUtilTest.java
@@ -0,0 +1,24 @@
+package pe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+class PEUtilTest
+{
+  @Test
+  public void testPrime()
+  {
+    assertTrue("five should be prime", PEUtil.isPrime(5));
+    assertFalse("twelve should not be prime", PEUtil.isPrime(12));
+    assertTrue("seventeen should be prime", PEUtil.isPrime(17));
+    assertFalse("FOURTEEN should not be prime", PEUtil.isPrime(14));
+    assertTrue("seventeen should be prime",PEUtil.isPrime(13));
+    assertTrue("three should be prime", PEUtil.isPrime(3));
+    assertFalse("21598L should not be prime", PEUtil.isPrime(21598L));
+    assertTrue("21599L should be prime", PEUtil.isPrime(21599L));
+    assertFalse("31599L should not be prime", PEUtil.isPrime(31599L));
+  }
+
+
+}
