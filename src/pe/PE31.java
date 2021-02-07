876af50d199bf154aee5b65df785ae3cd92f18eb
Daniel E. Chapman II <chapmand@gmail.com>
2021-02-07 16:35:31 -0500

    Setting up IntelliJ IDEA

--- src/pe/PE31.java
+++ src/pe/PE31.java
@@ -4,19 +4,21 @@ import java.util.*;
 
 public class PE31
 {
-  
+
   private static List<Integer> allButHigher(List<Integer> valids, int capoff)
   {
     List<Integer> newList = new ArrayList<Integer>();
     for (Integer valid : valids)
     {
       if (valid <= capoff)
-      newList.add(valid);
+      {
+        newList.add(valid);
+      }
     }
     return newList;
   }
 
-  static final List<Integer> validCurrencies = Arrays.asList(200, 100, 50,  20, 10, 5, 2, 1);
+  static final List<Integer> validCurrencies = Arrays.asList(200, 100, 50, 20, 10, 5, 2, 1);
 
   static long countValidTypes(int remaining, List<Integer> valids)
   {
@@ -32,7 +34,7 @@ public class PE31
       {
         if (validCurrency <= remaining)
         {
-          count += countValidTypes(remaining - validCurrency, allButHigher(validCurrencies,validCurrency));
+          count += countValidTypes(remaining - validCurrency, allButHigher(validCurrencies, validCurrency));
         }
       }
     }
@@ -47,5 +49,4 @@ public class PE31
   {                                        //200
     System.out.println("countVal = " + countValidTypes(200, validCurrencies));
   }
-
 }
