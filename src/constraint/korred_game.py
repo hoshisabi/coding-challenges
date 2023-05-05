1ee7e27456eef973bb3e3bef762d89c9b3cf43ee
Daniel E. Chapman II <chapmand@gmail.com>
2023-05-05 03:19:35 -0400

    added korred game from Kate's game tonight

---
+++ src/constraint/korred_game.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#
+# Korred puzzle, 9 rocks in squares, need rearranged to satisfy the rows/columns numbers
+#
+#   3 1 2 3
+# 3 O O O .
+# 2 O O O .
+# 3 O O O .
+# 1 . . . .
+
+
+import constraint
+
+xvals = [3, 2, 3, 1]
+yvals = [3, 1, 2, 3]
+
+def print_board(d):
+    s = "  "
+    for v in yvals:
+        s += str(v) + " "
+    s += "\n"
+    for x in range(4):
+        s += str(xvals[x]) + " "
+        for y in range(4):
+            s += "O " if d[(x, y)] == 1 else ". "
+        s += "\n"
+    print(s)
+
+def main():
+    problem = constraint.Problem()
+    coords = [(x, y) for x in range(4) for y in range(4)]
+    problem.addVariables(coords, range(2))
+    problem.addConstraint(constraint.ExactSumConstraint(9))
+
+    for y in range(4):
+        problem.addConstraint(constraint.ExactSumConstraint(xvals[y]), [(x, y) for x in range(4)])
+    for x in range(4):
+        problem.addConstraint(constraint.ExactSumConstraint(yvals[x]), [(x, y) for y in range(4)])
+
+    sols = problem.getSolutions()
+    for s in sols:
+        print_board(s)
+
+    print(f"Exactly {len(sols)} solutions")
+
+if __name__ == "__main__":
+    main()
