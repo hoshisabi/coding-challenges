b1708015cdc91b20a85e29e6c01515dd80a6f0c4
Daniel E. Chapman II <chapmand@gmail.com>
2023-05-05 03:29:04 -0400

    fixed x,y legend printing, added ability to change number of rocks

--- src/constraint/korred_game.py
+++ src/constraint/korred_game.py
@@ -3,24 +3,25 @@
 # Korred puzzle, 9 rocks in squares, need rearranged to satisfy the rows/columns numbers
 #
 #   3 1 2 3
-# 3 O O O .
-# 2 O O O .
-# 3 O O O .
+# 3 . . . .
+# 2 . . . .
+# 3 . . . .
 # 1 . . . .
 
 
 import constraint
 
-xvals = [3, 2, 3, 1]
-yvals = [3, 1, 2, 3]
+xvals = [3, 1, 2, 3]
+yvals = [3, 2, 3, 1]
+rocks = 9
 
 def print_board(d):
     s = "  "
-    for v in yvals:
+    for v in xvals:
         s += str(v) + " "
     s += "\n"
     for x in range(4):
-        s += str(xvals[x]) + " "
+        s += str(yvals[x]) + " "
         for y in range(4):
             s += "O " if d[(x, y)] == 1 else ". "
         s += "\n"
@@ -30,7 +31,7 @@ def main():
     problem = constraint.Problem()
     coords = [(x, y) for x in range(4) for y in range(4)]
     problem.addVariables(coords, range(2))
-    problem.addConstraint(constraint.ExactSumConstraint(9))
+    problem.addConstraint(constraint.ExactSumConstraint(rocks))
 
     for y in range(4):
         problem.addConstraint(constraint.ExactSumConstraint(xvals[y]), [(x, y) for x in range(4)])
