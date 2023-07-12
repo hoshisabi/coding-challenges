5ff1be03a1f7ae6deb80622e7e20dc8e9579076e
Daniel E. Chapman II <chapmand@gmail.com>
2023-07-12 00:33:04 -0400

    Added john's puzzles that he had mentioned

---
+++ src/constraint/john_coin_puzzle_stricter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#
+# puzzle:
+# 2 5 2
+# 5 _ 5     (max sum 28)
+# 2 5 2
+# can be turned into
+# 4 1 4
+# 1 _ 1     (max sum 20)
+# 4 1 4
+# max sum of
+# constraint:
+# outer rows/columns exact sum of 9
+# middle cell must be 0
+# outer cells must be 1 or greater
+# total of all cells must be less than 28
+# win condition, minimum total of all cells
+# Cells:
+# OEO
+# E_E
+# OEO
+
+from constraint import *
+
+expected_sum = 9
+total_sum_max = 28
+
+def main():
+    problem = Problem()
+    problem.addVariables("oe", range(1,10))
+    problem.addConstraint(lambda o, e: 2 * o + e == expected_sum, "oe")
+
+    sols = problem.getSolutions()
+    min_sum = 99
+    poss_sol = {}
+    for sol in sols:
+        sol_sum = 4 * sol['o'] + 4 * sol['e']
+        if sol_sum < min_sum:
+            min_sum = sol_sum
+            poss_sol = sol
+
+    print(f"Minimum sum {min_sum}")
+    print(f"{poss_sol['o']}{poss_sol['e']}{poss_sol['o']}\n"
+          f"{poss_sol['e']}_{poss_sol['e']}\n"
+          f"{poss_sol['o']}{poss_sol['e']}{poss_sol['o']}")
+
+
+if __name__ == "__main__":
+    main()
