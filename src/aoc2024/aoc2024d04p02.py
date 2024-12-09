725b563232f0508e888fe874cd1d8758e031a93d
Daniel E. Chapman II <chapmand@gmail.com>
2024-12-09 07:04:35 -0500

    Part 4

--- src/aoc2024/aoc2024d04p02.py
+++ src/aoc2024/aoc2024d04p02.py
@@ -1,46 +1,37 @@
 from aoc2024d04_util import *
 
+
 def read_grid_from_file(filename):
     lines = []
     with open(filename, 'r') as file:
         lines.extend(line.strip() for line in file)
     return lines
 
+
 def is_valid(grid, c, x, y):
     return 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == c
 
 
 word_cross_directions = [
-    (0, 1, 1, 0, 0, -1, -1, 0),  # m = right, m = down
-    (0, -1, 1, 0, 0, 1, -1, 0),  # m = left, m = down
-    (0, 1, -1, 0, 0, -1, 1, 0),  # m = right, m = up
-    (0, -1, -1, 0, 0, 1, 1, 0),  # m = left, m = up
-    (1, -1, 1, 1, -1, 1, -1, -1),  # m = left-down diagonal, m = right-down diagonal
-    (1, -1, -1, -1, -1, 1, 1, 1),  # m = left-down diagonal, m = left-up diagonal
-    (1, 1, -1, 1, -1, -1, 1, -1),  # m = right-down diagonal, m = right-up diagonal
-    (1, 1, -1, -1, -1, -1, 1, 1),  # m = right-down diagonal, m = left-up diagonal
+    (-1, -1, -1, 1, 1, -1, 1, 1), # M up-left, M up-right, S down-left, S down-right
+    (-1, -1, 1, -1, -1, 1, 1, 1), # M up-left, M down-left, S up-right, S down-right
+    (-1, 1, 1, 1, 1, -1, -1, -1), # M up-right, M down-right, S down-left, S up-left
+    (1, -1, 1, 1, -1, -1, -1, 1), # M down-left, M down-right, S up-left, S up-right
 ]
-
 
 # Solve the puzzle with input from file
 def solve_puzzle(filename):  # sourcery skip: use-itertools-product
     grid = read_grid_from_file(filename)
+    a_indices = [(x, y) for x, row in enumerate(grid) for y, char in enumerate(row) if char == 'A']
     count = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if is_valid(grid, 'A', x, y):
-                count = check_cross(count, grid)
+    for (x, y) in a_indices:
+        for c in word_cross_directions:
+            if (is_valid(grid, 'M', x + c[0], y + c[1]) and
+                    is_valid(grid, 'M', x + c[2], y + c[3]) and
+                    is_valid(grid, 'S', x + c[4], y + c[5]) and
+                    is_valid(grid, 'S', x + c[6], y + c[7])):
+                count += 1
     return count
-
-
-def check_cross(count, grid):
-    for m1x, m1y, m2x, m2y, s1x, s1y, s2x, s2y in word_cross_directions:
-        if is_valid(grid, 'M', m1x, m1y) and is_valid(grid, 'S', s1x, s2y) and is_valid(grid, 'M', m2x,
-                                                                                        m2y) and is_valid(
-                grid, 'S', s2x, s2y):
-            count += 1
-    return count
-
 
 # Test data:
 test_result = solve_puzzle("aoc2024d04_test_input.txt")
