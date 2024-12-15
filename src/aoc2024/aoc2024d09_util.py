26f054a3dd5e6a086dec9b43896464517b878518
Daniel E. Chapman II <chapmand@gmail.com>
2024-12-15 06:51:25 -0500

    added more optimal solution for day 9 part 1

---
+++ src/aoc2024/aoc2024d09_util.py
@@ -0,0 +1,27 @@
+from itertools import batched
+
+def disk_map_to_string(disk_map):
+    return "".join(f"[{block}]" if block is not None else "[.]" for block in disk_map)
+
+def read_file(filename):
+    block_map = []
+
+    # we only read the first line, if there are multiple.
+    # it should only be one line
+    with open(filename, 'r') as file:
+        disk_string = file.readline().strip()
+
+    for file_id, pair in enumerate(batched(disk_string, 2)):
+        f, s = 0, 0
+        if len(pair) == 2:
+            f, s = map(int, pair)
+        else:
+            f = int(pair[0])
+
+        for _ in range(f):
+            block_map += [file_id]
+
+        for _ in range(s):
+            block_map += [ None ]
+
+    return block_map
